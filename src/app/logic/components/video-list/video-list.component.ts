6d4d6b1276882e63284db170a3b6be8d4ceda49a
archiewald <artur.kozubek1@gmail.com>
2019-02-24 15:36:07 +0100

    implement video-list

--- src/app/logic/components/video-list/video-list.component.ts
+++ src/app/logic/components/video-list/video-list.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Video } from "../../models/video";
 
 @Component({
-  selector: 'app-video-list',
-  templateUrl: './video-list.component.html',
-  styleUrls: ['./video-list.component.css']
+  selector: "app-video-list",
+  templateUrl: "./video-list.component.html",
+  styleUrls: ["./video-list.component.css"]
 })
-export class VideoListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class VideoListComponent {
+  @Input() videos: Video[] = null;
 }
