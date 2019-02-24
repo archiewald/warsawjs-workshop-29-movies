39d8f2c72426de580806c92b59c3679353b9cad7
archiewald <artur.kozubek1@gmail.com>
2019-02-24 12:31:58 +0100

    restructure folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { BackgroundColorDirective } from "./background-color.directive";
+import { BackgroundColorDirective } from "./shared/directives/background-color.directive";
 import { SharedModule } from "./shared/shared.module";
 import { LogicModule } from "./logic/logic.module";
 import { RouterModule } from "@angular/router";
