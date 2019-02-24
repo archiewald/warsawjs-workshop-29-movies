23ce689d0544622f112bb4c610bc2aac5a3cc04f
archiewald <artur.kozubek1@gmail.com>
2019-02-24 11:22:08 +0100

    add gh-pages deploy, background color directive, menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackgroundColorDirective } from './background-color.directive';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundColorDirective,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
