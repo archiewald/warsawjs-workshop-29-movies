b8d6ab27fa926881268514c2dbbded7c0ef97efd
archiewald <artur.kozubek1@gmail.com>
2019-02-24 14:53:38 +0100

    try to make it work on gh-pages

--- src/app/logic/services/videos.service.ts
+++ src/app/logic/services/videos.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
+import { environment } from "../../../environments/environment";
+
 @Injectable({
   providedIn: "root"
 })
 export class VideosService {
   constructor(private http: HttpClient) {}
 
-  async fetchVideos() {
-    return await this.http.get("/assets/data.json").toPromise();
+  fetchVideos() {
+    return this.http.get(environment.backendURL).toPromise();
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  backendURL: "warsawjs-workshop-29-movies/assets/data.json"
 };
