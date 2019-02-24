7f9f82c5d7d1b29e9fa58ffae8fbd0cf610265fd
archiewald <artur.kozubek1@gmail.com>
2019-02-24 12:06:52 +0100

    add page home component

---
+++ src/app/logic/logic.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageHomeComponent } from '../page-home/page-home.component';
+
+@NgModule({
+  declarations: [PageHomeComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LogicModule { }
