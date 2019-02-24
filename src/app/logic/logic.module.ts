8830cd654bfa1b93fb145891bf00be5cd0b7c5c8
archiewald <artur.kozubek1@gmail.com>
2019-02-24 12:08:03 +0100

    add page contact

--- src/app/logic/logic.module.ts
+++ src/app/logic/logic.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageHomeComponent } from '../page-home/page-home.component';
+import { PageContactComponent } from '../page-contact/page-contact.component';
 
 @NgModule({
-  declarations: [PageHomeComponent],
+  declarations: [PageHomeComponent, PageContactComponent],
   imports: [
     CommonModule
   ]
